da211a6dd6eb7d5d3762d7b7623e7e7deff5a5af
Geovanny Nascimento Silva <geovanny.gns1@gmail.com>
2023-06-05 11:49:33 -0300

    Initializing the repository and creating the splash screen

---
+++ src/javascript/main.js
@@ -0,0 +1,32 @@
+// API
+
+// https://rapidapi.com/SAdrian/api/moviesdatabase
+
+const requestApi = async () => {
+
+    const url = 'https://moviesdatabase.p.rapidapi.com/titles/random?list=most_pop_movies';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4d22f61055msh339bba6dd0edd70p10eb88jsn9d4271618ebc',
+            'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+
+        // const img = document.createElement("img");
+        // img.src = result.results[0].primaryImage.url;
+        // document.body.appendChild(img);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+requestApi();
