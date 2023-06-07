b3eb78f44cd664be24b5649233341b052234eecf
Geovanny Nascimento Silva <geovanny.gns1@gmail.com>
2023-06-06 22:34:22 -0300

    Addition of the 'How To Play' modal, Construction of the 'tableGame' table and start of construction of the 'easterEgg'

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,7 +1,22 @@
+// Abrir o modal How To Play
+
+const clickBottunHowToPlay = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "inline";
+}
+
+// Fechar o modal How To Play
+
+const closeModal = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "none";
+}
+
 // Mudança de tela 
 
 const clickBottunPlay = () => {
     window.location.href = "game.html";
+    requestApi();
 }
 
 // API
@@ -32,4 +47,3 @@ const requestApi = async () => {
         console.error(error);
     }
 }
-requestApi();
